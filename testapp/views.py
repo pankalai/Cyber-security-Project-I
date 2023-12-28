28de379d37ab079eed7dc6655d4f322a8ba02c4e
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-12-28 16:37:34 +0200

    fixes

--- testapp/views.py
+++ testapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 
-from re import compile, search
 import sqlite3
+
+#from django.contrib.auth.hashers import make_password, check_password
+#from re import compile, search
 
 
 def index(request,notes=None):
@@ -34,8 +36,6 @@ def register(request):
         """
         BROKEN AUTHENTICATION
 
-        Weak passwords are easy to crack by experimenting.
-
         The following requires a password of at least eight characters, 
         with at least one lowercase letter, one uppercase letter and one number.
 
@@ -44,16 +44,18 @@ def register(request):
         valid = search(pattern, password)
         if not valid:
             return HttpResponse("Password too weak. Password must contain at least eight characters, with at least one lowercase letter, one uppercase letter and one number.")
+
+        hashed_pwd = make_password(password)
         """
        
-        text = """
+        sql = """
             INSERT INTO users (first_name,last_name,username,password,credit_card,admin) 
             VALUES  (?,?,?,?,?,?)
         """
 
         conn = sqlite3.connect('db.sqlite')
         cursor = conn.cursor()
-        cursor.execute(text, (first_name,last_name,username,password,credit_card,False))
+        cursor.execute(sql, (first_name,last_name,username,password,credit_card,False))
         conn.commit()
         return redirect("/login")    
 
@@ -67,8 +69,8 @@ def login(request):
 
         conn = sqlite3.connect('db.sqlite')
         cursor = conn.cursor()
-        text = "SELECT id,password,admin FROM users where username = ?"
-        user = cursor.execute(text,[username]).fetchone()
+        sql = "SELECT id,password,admin FROM users where username = ?"
+        user = cursor.execute(sql,[username]).fetchone()
   
         if not user:
             return HttpResponse("Unknown user")
@@ -88,13 +90,9 @@ def logout(request):
 
 def admin(request):
     """
-    SECURITY MISCONFIGURATION
+    BROKEN ACCESS CONTROL
 
-    Without checking rights, all users have access
-    for a page intended for admins, which have
-    information about all of the users.
-
-    Adding a simple check this can be avoided.
+    User rights check should be added:
 
     if not is_admin(request):
         raise PermissionDenied()
@@ -110,21 +108,27 @@ def get(request):
     cursor = conn.cursor()
     
     if note:
-        text = "SELECT user_id, note FROM notes WHERE user_id = '%s' and note like '%%%s%%'" % (
+        sql = "SELECT user_id, note FROM notes WHERE user_id = '%s' and note like '%%%s%%'" % (
             user_id,
             note
         )
+        """
+        SHOULD BE:
+        sql = "SELECT user_id, note FROM notes WHERE user_id = ? and note like ?"
+        content = cursor.execute(sql, [user_id,'%'+note+'%']).fetchall()
+        """
     else:
-        text = "SELECT user_id, note FROM notes WHERE user_id = '%s'" % user_id
+        sql = "SELECT user_id, note FROM notes WHERE user_id = '%s'" % user_id
+        
+    content = cursor.execute(sql).fetchall()
 
-    content = cursor.execute(text).fetchall()
 
     """
     SENSITIVE DATA EXPOSURE
 
     With sql injection vulnerability existing, input
     ' UNION SELECT 1,credit_card FROM users WHERE first_name like 'sam
-    returns credit card number of another user unencrypted
+    returns credit card number of another user
 
     It must be encrypted before writing to the database,
     for example by using Fernet
@@ -146,6 +150,7 @@ def get(request):
 
 def add(request):
     note = request.GET.get('note')
+    
     user_id = str(request.session.get("user_id", None))
 
     if not note:
@@ -154,24 +159,24 @@ def add(request):
     conn = sqlite3.connect('db.sqlite')
     cursor = conn.cursor()
 
-    text = "INSERT INTO notes (user_id,note) VALUES (" + user_id + ",'" + note + "')"
-
-    cursor.executescript(text)
-    conn.commit()
+    sql = "INSERT INTO notes (user_id, note) VALUES (" + user_id + ",'" + note + "')"
+    cursor.executescript(sql)
+    
 
     """
     INJECTION
 
-    For example, with 'add' input
-    '); DELETE FROM notes WHERE ('1'='1
-    command clears table 'notes' 
+    Input
+    a'); DELETE FROM notes WHERE ('1'='1
+    clears the table 'notes' 
 
-    By providing input as a parameter and using the execute method,
-    which executes only one command, the problem disappears
+    Input should be provided as a parameter:
 
-    text = "INSERT INTO notes (note) VALUES (?)"
-    cursor.execute(text, [note])
+    sql = "INSERT INTO notes (user_id, note) VALUES (?,?)"
+    cursor.execute(sql, (user_id, note))
     """
+
+    conn.commit()
     return redirect("/")
 
 def csrf(request):
@@ -180,19 +185,19 @@ def csrf(request):
 def get_users():
     conn = sqlite3.connect('db.sqlite')
     cursor = conn.cursor()
-    text = "SELECT first_name, last_name, username, credit_card FROM users"
-    users = cursor.execute(text).fetchall()
+    sql = "SELECT first_name, last_name, username, credit_card FROM users"
+    users = cursor.execute(sql).fetchall()
     return users
 
 def get_notes(user_id):
     conn = sqlite3.connect('db.sqlite')
     cursor = conn.cursor()
-    text = "SELECT * FROM notes WHERE user_id = ?"
-    content = cursor.execute(text, [user_id]).fetchall()
+    sql = "SELECT * FROM notes WHERE user_id = ?"
+    content = cursor.execute(sql, [user_id]).fetchall()
     return content
 
-def is_admin(request):
-    return request.session.get("admin", False)
+# def is_admin(request):
+#     return request.session.get("admin", False)
 
 
 
